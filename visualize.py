e2c8d18d429183e70ceea08b9ee338a1eb0a6eeb
smitdumore <smitd@umd.edu>
2025-02-08 12:07:48 -0500

    added visualization files

--- visualize.py
+++ visualize.py
@@ -1,143 +1,173 @@
+#!/usr/bin/env python3
+import yaml
+import matplotlib
+# matplotlib.use("Agg")
+from matplotlib.patches import Circle, Rectangle, Arrow
+from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Rectangle
+import numpy as np
 from matplotlib import animation
-import json
-import random
+import matplotlib.animation as manimation
+import argparse
+import math
+
+Colors = ['orange']#, 'blue', 'green']
+
 
 class Animation:
-    def __init__(self, map_json, solution_json):
-        self.map = map_json
-        self.solution = solution_json
+  def __init__(self, map, schedule):
+    self.map = map
+    self.schedule = schedule
 
-        # Prepare figure
-        aspect = self.map["dimX"] / self.map["dimY"]
-        self.fig = plt.figure(frameon=False, figsize=(4*aspect, 4))
-        self.ax = self.fig.add_subplot(111, aspect='equal')
-        self.fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    aspect = map["map"]["dimensions"][0] / map["map"]["dimensions"][1]
 
-        self.patches = []
-        self.agents = {}
-        self.agent_colors = {}
+    self.fig = plt.figure(frameon=False, figsize=(4 * aspect, 4))
+    self.ax = self.fig.add_subplot(111, aspect='equal')
+    self.fig.subplots_adjust(left=0,right=1,bottom=0,top=1, wspace=None, hspace=None)
+    # self.ax.set_frame_on(False)
 
-        # Set up the grid boundary
-        xmin, ymin = -0.5, -0.5
-        xmax, ymax = self.map["dimX"] - 0.5, self.map["dimY"] - 0.5
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
+    self.patches = []
+    self.artists = []
+    self.agents = dict()
+    self.agent_names = dict()
+    # create boundary patch
+    xmin = -0.5
+    ymin = -0.5
+    xmax = map["map"]["dimensions"][0] - 0.5
+    ymax = map["map"]["dimensions"][1] - 0.5
 
-        # Border rectangle for the entire grid
-        self.patches.append(
-            Rectangle((xmin, ymin),
-                      xmax - xmin,
-                      ymax - ymin,
-                      facecolor="none",
-                      edgecolor='black')
-        )
+    # self.ax.relim()
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
+    # self.ax.set_xticks([])
+    # self.ax.set_yticks([])
+    # plt.axis('off')
+    # self.ax.axis('tight')
+    # self.ax.axis('off')
 
-        # Draw obstacles
-        for (ox, oy) in self.map["obstacles"]:
-            rect = Rectangle((ox - 0.5, oy - 0.5),
-                             1, 1,
-                             facecolor='red',
-                             edgecolor='black')
-            self.patches.append(rect)
+    self.patches.append(Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, facecolor='none', edgecolor='red'))
+    for o in map["map"]["obstacles"]:
+      x, y = o[0], o[1]
+      self.patches.append(Rectangle((x - 0.5, y - 0.5), 1, 1, facecolor='red', edgecolor='red'))
 
-        # Random color per agent
-        num_agents = len(self.map["starts"])
-        for agent_id in range(num_agents):
-            color = (random.random(), random.random(), random.random())
-            self.agent_colors[agent_id] = color
+    # create agents:
+    self.T = 0
+    # draw goals first
+    for d, i in zip(map["agents"], range(0, len(map["agents"]))):
+      if "goal" in d:
+        goals = [d["goal"]]
+      if "potentialGoals" in d:
+        goals = [goal for goal in d["potentialGoals"]]
+      for goal in goals:
+        self.patches.append(Rectangle((goal[0] - 0.25, goal[1] - 0.25), 0.5, 0.5, facecolor=Colors[i%len(Colors)], edgecolor='black', alpha=0.5))
 
-        # Draw goals (match each goal color to its agent)
-        for i, g in enumerate(self.map["goals"]):
-            gx, gy = g
-            goal_rect = Rectangle((gx - 0.4, gy - 0.4),
-                                  0.8, 0.8,
-                                  facecolor=self.agent_colors[i],
-                                  edgecolor='black')
-            self.patches.append(goal_rect)
+    for d, i in zip(map["agents"], range(0, len(map["agents"]))):
+      name = d["name"]
+      self.agents[name] = Circle((d["start"][0], d["start"][1]), 0.3, facecolor=Colors[i%len(Colors)], edgecolor='black')
+      self.agents[name].original_face_color = Colors[i%len(Colors)]
+      self.patches.append(self.agents[name])
+      self.T = max(self.T, schedule["schedule"][name][-1]["t"])
+      self.agent_names[name] = self.ax.text(d["start"][0], d["start"][1], name.replace('agent', ''))
+      self.agent_names[name].set_horizontalalignment('center')
+      self.agent_names[name].set_verticalalignment('center')
+      self.artists.append(self.agent_names[name])
 
-        # Place agents at their start positions
-        self.max_path_length = 0
-        for i, start_pos in enumerate(self.map["starts"]):
-            sx, sy = start_pos
-            circle = Circle((sx, sy),
-                            0.3,
-                            facecolor=self.agent_colors[i],
-                            edgecolor='black')
-            self.agents[i] = circle
-            self.patches.append(circle)
 
-            path_len = len(self.solution['solutions'][i]['path'])
-            self.max_path_length = max(self.max_path_length, path_len)
 
-        # We want sub-frames for smooth dragging:
-        self.frames_per_step = 20  # how many frames to "move" one cell
-        # If max_path_length = L, we have (L-1) moves. Each move gets frames_per_step.
-        # Add +1 to handle the final position.
-        self.total_frames = (self.max_path_length - 1) * self.frames_per_step + self.frames_per_step
+    # self.ax.set_axis_off()
+    # self.fig.axes[0].set_visible(False)
+    # self.fig.axes.get_yaxis().set_visible(False)
 
-        # Create animation
-        self.animation = animation.FuncAnimation(
-            self.fig,
-            self.animate_func,
-            init_func=self.init_func,
-            frames=self.total_frames,
-            interval=100,   # ms between frames; increase if you want even slower motion
-            blit=True
-        )
+    # self.fig.tight_layout()
 
-    def init_func(self):
-        # Add all patches to the axes
-        for patch in self.patches:
-            self.ax.add_patch(patch)
-        return self.patches
+    self.anim = animation.FuncAnimation(self.fig, self.animate_func,
+                               init_func=self.init_func,
+                               frames=int(self.T+1) * 10,
+                               interval=100,
+                               blit=True)
 
-    def animate_func(self, frame):
-        """
-        frame goes from 0 to total_frames-1.
-        We figure out which segment of the path we're in (t),
-        how far along that segment we are (substep), and interpolate.
-        """
-        t = frame // self.frames_per_step       # which segment (integer)
-        substep = frame % self.frames_per_step  # sub‐frame within that segment
-        alpha = substep / float(self.frames_per_step)  # fraction [0..1]
+  def save(self, file_name, speed):
+    self.anim.save(
+      file_name,
+      "ffmpeg",
+      fps=10 * speed,
+      dpi=200),
+      # savefig_kwargs={"pad_inches": 0, "bbox_inches": "tight"})
 
-        for agent_id, circle in self.agents.items():
-            path = self.solution['solutions'][agent_id]['path']
-            # If there's only 1 coordinate, do nothing
-            if len(path) < 2:
-                # Stays at the start forever
-                circle.center = path[0]
-                continue
+  def show(self):
+    plt.show()
 
-            # If we've exceeded the path length, just stay at the last position
-            if t >= len(path) - 1:
-                circle.center = path[-1]
-            else:
-                # Interpolate from path[t] to path[t+1]
-                (x1, y1) = path[t]
-                (x2, y2) = path[t+1]
-                x_pos = x1 + alpha * (x2 - x1)
-                y_pos = y1 + alpha * (y2 - y1)
-                circle.center = (x_pos, y_pos)
+  def init_func(self):
+    for p in self.patches:
+      self.ax.add_patch(p)
+    for a in self.artists:
+      self.ax.add_artist(a)
+    return self.patches + self.artists
 
-        return self.patches
+  def animate_func(self, i):
+    for agent_name in self.schedule["schedule"]:
+      agent = schedule["schedule"][agent_name]
+      pos = self.getState(i / 10, agent)
+      p = (pos[0], pos[1])
+      self.agents[agent_name].center = p
+      self.agent_names[agent_name].set_position(p)
 
-    def show(self):
-        plt.show()
+    # reset all colors
+    for _,agent in self.agents.items():
+      agent.set_facecolor(agent.original_face_color)
+
+    # check drive-drive collisions
+    agents_array = [agent for _,agent in self.agents.items()]
+    for i in range(0, len(agents_array)):
+      for j in range(i+1, len(agents_array)):
+        d1 = agents_array[i]
+        d2 = agents_array[j]
+        pos1 = np.array(d1.center)
+        pos2 = np.array(d2.center)
+        if np.linalg.norm(pos1 - pos2) < 0.7:
+          d1.set_facecolor('red')
+          d2.set_facecolor('red')
+          print("COLLISION! (agent-agent) ({}, {})".format(i, j))
+
+    return self.patches + self.artists
+
+
+  def getState(self, t, d):
+    idx = 0
+    while idx < len(d) and d[idx]["t"] < t:
+      idx += 1
+    if idx == 0:
+      return np.array([float(d[0]["x"]), float(d[0]["y"])])
+    elif idx < len(d):
+      posLast = np.array([float(d[idx-1]["x"]), float(d[idx-1]["y"])])
+      posNext = np.array([float(d[idx]["x"]), float(d[idx]["y"])])
+    else:
+      return np.array([float(d[-1]["x"]), float(d[-1]["y"])])
+    dt = d[idx]["t"] - d[idx-1]["t"]
+    t = (t - d[idx-1]["t"]) / dt
+    pos = (posNext - posLast) * t + posLast
+    return pos
+
 
 
 if __name__ == "__main__":
-    # EXAMPLE: Even if a path has only 2 steps, it will be dragged across sub-frames.
-    # Use your actual JSON files if you have them.
-    #
-    # Here is a minimal example of a 'small' path for each of two agents:
+  parser = argparse.ArgumentParser()
+  parser.add_argument("map", help="input file containing map")
+  parser.add_argument("schedule", help="schedule for agents")
+  parser.add_argument('--video', dest='video', default=None, help="output video file (or leave empty to show on screen)")
+  parser.add_argument("--speed", type=int, default=1, help="speedup-factor")
+  args = parser.parse_args()
 
-    with open("test/sampleSchedule.json") as solution_json_file:
-        solution_json = json.load(solution_json_file)
-    with open("test/sampleMap.json") as map_json_file:
-        map_json = json.load(map_json_file)
 
-    anim = Animation(map_json, solution_json)
-    anim.show()
+  with open(args.map) as map_file:
+    map = yaml.safe_load(map_file)
+
+  with open(args.schedule) as states_file:
+    schedule = yaml.safe_load(states_file)
+
+  animation = Animation(map, schedule)
+
+  if args.video:
+    animation.save(args.video, args.speed)
+  else:
+    animation.show()
